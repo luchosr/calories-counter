ac283d48e75bec7046e1c8f2a87bfe518515b472
Luciano Ramello <luchosr@gmail.com>
2024-08-26 17:32:29 +0200

    feat: adds form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,48 @@
+import { categories } from '../data/categories';
+
+export default function Form() {
+  return (
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className="font-bold">
+          Categoría:
+        </label>
+        <select className="border border-slate-300 p-2 rounded-lg w-full bg-white">
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className="font-bold">
+          Actividad:
+        </label>
+        <input
+          type="text"
+          id="activity"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. Comida, jugo de naranja, Ejercicio, Pesas, Bicicleta"
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">
+          Calorías:
+        </label>
+        <input
+          type="number"
+          id="calories"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Calorías: ej. 300 o 500"
+        />
+      </div>
+      <input
+        type="submit"
+        id="calories"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer "
+        value="Guardar Comida o Ejercicio"
+      />
+    </form>
+  );
+}
