2164b6effe3f0e01f3451bc38169fbf29835b1c7
Luciano Ramello <luchosr@gmail.com>
2024-08-26 17:32:42 +0200

    feat: implements form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Form from './components/Form';
+
 function App() {
   return (
     <>
@@ -8,6 +10,11 @@ function App() {
           </h1>
         </div>
       </header>
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form />
+        </div>
+      </section>
     </>
   );
 }
