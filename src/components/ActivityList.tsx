22fb8c6281e16206f2a18deda0b2854935c0242d
Luciano Ramello <luchosr@gmail.com>
2024-08-28 16:48:25 +0200

    feat: adds category name implementation

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,10 +1,18 @@
 import { Activity } from '../types';
+import { categories } from '../data/categories';
+import { useMemo } from 'react';
+import { PencilSquareIcon } from '@heroicons/react/24/solid';
 
 type ActivityListProps = {
   activities: Activity[];
 };
 
 export default function ActivityList({ activities }: ActivityListProps) {
+  const categoryName = useMemo(
+    () => (category: Activity['category']) =>
+      categories.map((cat) => (cat.id === category ? cat.name : '')),
+    [activities]
+  );
   return (
     <>
       <h2 className="text-4xl font-bold text-slate-600 text-center">
@@ -17,14 +25,24 @@ export default function ActivityList({ activities }: ActivityListProps) {
           key={activity.id}
         >
           <div className="space-y-2 relative">
-            <p className="">{activity.category}</p>
+            <p
+              className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${
+                activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'
+              }`}
+            >
+              {categoryName(+activity.category)}
+            </p>
             <p className="text-2xl font-bold pt-5">{activity.name}</p>
             <p className="font-black text-4xl text-lime-500">
               {activity.calories} <span>Calorías</span>
             </p>
             {activity.name}
           </div>
-          <div className=""></div>
+          <div className="flex gap-5 items-center">
+            <button>
+              <PencilSquareIcon className="h-8 w-8 text-gray-800" />
+            </button>
+          </div>
         </div>
       ))}
     </>
