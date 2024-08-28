c5b46928e772c01a319b232f346924a891f10bcc
Luciano Ramello <luchosr@gmail.com>
2024-08-28 19:02:27 +0200

    feat: adds locals storage activities setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import Form from './components/Form';
 import { activityReducer, initialState } from './reducers/activity-reducer';
 import ActivityList from './components/ActivityList';
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem('activities', JSON.stringify(state.activities));
+  }, [state.activities]);
+
   return (
     <>
       <header className="bg-lime-600 py-3">
