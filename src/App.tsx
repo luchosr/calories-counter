2eafe757a4a13e2c1e14e7dc686ea3418ca2e53a
Luciano Ramello <luchosr@gmail.com>
2024-08-29 11:34:36 +0200

    style: improves app reset button classes

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,13 @@ function App() {
   return (
     <>
       <header className="bg-lime-600 py-3">
-        <div className="max-w-4xl mx-auto flex justify-between">
+        <div className="max-w-4xl items-center mx-auto flex justify-between">
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Contador de Calorías
           </h1>
 
           <button
-            className="bg-gray-800 hover:bg-gray-900 p2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
+            className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
             disabled={!CanRestartApp()}
             onClick={() => dispatch({ type: 'restart-app' })}
           >
