0a5ed937200ac723dd62e0d26efb3e70a774b2af
Luciano Ramello <luchosr@gmail.com>
2024-08-29 11:32:41 +0200

    feat: implements total calories calculation

--- src/components/CaloryTracker.tsx
+++ src/components/CaloryTracker.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from 'react';
 import type { Activity } from '../types';
+import CaloryDisplay from './CaloryDisplay';
 
 type CaloryTrackerProps = {
   activities: Activity[];
@@ -15,18 +16,31 @@ export default function CaloryTracker({ activities }: CaloryTrackerProps) {
       ),
     [activities]
   );
+
+  const caloriesBurned = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 2 ? total + +activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+
+  const netCalories = useMemo(
+    () => caloriesConsumed - caloriesBurned,
+    [activities]
+  );
   return (
     <>
       <h2 className="text-4xl font-black text-white text-center">
         Resumen de calorías
       </h2>
       <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
-        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-          <span className="font-black text-6xl text-orange">
-            {caloriesConsumed}
-          </span>
-          Consumidas
-        </p>
+        <CaloryDisplay calories={caloriesConsumed} text={'Consumidas'} />
+
+        <CaloryDisplay calories={caloriesBurned} text={'Calorías quemadas'} />
+        <CaloryDisplay calories={netCalories} text={'Calorías totales'} />
       </div>
     </>
   );
