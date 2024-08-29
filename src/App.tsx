367c329c11aae77331748b299a211cf9cc25f9db
Luciano Ramello <luchosr@gmail.com>
2024-08-29 09:18:05 +0200

    feat: implements Calory Tracker component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useReducer } from 'react';
 import Form from './components/Form';
 import { activityReducer, initialState } from './reducers/activity-reducer';
 import ActivityList from './components/ActivityList';
+import CaloryTracker from './components/CaloryTracker';
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
@@ -32,6 +33,11 @@ function App() {
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} state={state} />
+        </div>
+      </section>
+      <section className="bg-gray-800 py-10">
+        <div className="max-w-4xl mx-auto">
+          <CaloryTracker activities={state.activities} />
         </div>
       </section>
       <section className="p-10 mx-auto max-w-4xl">
