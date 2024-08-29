f45953db8b03a2a25c91f53030865ded4b784a5a
Luciano Ramello <luchosr@gmail.com>
2024-08-29 11:33:03 +0200

    feat: adds Calory Display component

---
+++ src/components/CaloryDisplay.tsx
@@ -0,0 +1,12 @@
+type CaloryDisplayprops = {
+  calories: number;
+  text: string;
+};
+export default function CaloryDisplay({ calories, text }: CaloryDisplayprops) {
+  return (
+    <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+      <span className="font-black text-6xl text-orange">{calories}</span>
+      {text}
+    </p>
+  );
+}
