17291943c1c752823dacdddd8e2ba00da7666c40
Luciano Ramello <luchosr@gmail.com>
2024-08-29 10:16:45 +0200

    feat: adds calories consumed

--- src/components/CaloryTracker.tsx
+++ src/components/CaloryTracker.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import type { Activity } from '../types';
 
 type CaloryTrackerProps = {
@@ -5,5 +6,28 @@ type CaloryTrackerProps = {
 };
 
 export default function CaloryTracker({ activities }: CaloryTrackerProps) {
-  return <div>CaloryTracker</div>;
+  const caloriesConsumed = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 1 ? total + +activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+  return (
+    <>
+      <h2 className="text-4xl font-black text-white text-center">
+        Resumen de calorías
+      </h2>
+      <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+          <span className="font-black text-6xl text-orange">
+            {caloriesConsumed}
+          </span>
+          Consumidas
+        </p>
+      </div>
+    </>
+  );
 }
